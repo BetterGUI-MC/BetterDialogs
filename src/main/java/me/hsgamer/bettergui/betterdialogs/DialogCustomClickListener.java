455545d1ba90f86d53553de095eccee1e3f547fd
hsgamer <huynhqtienvtag@gmail.com>
2025-06-27 13:23:23 +0700

    the fix

--- src/main/java/me/hsgamer/bettergui/betterdialogs/DialogCustomClickListener.java
+++ src/main/java/me/hsgamer/bettergui/betterdialogs/DialogCustomClickListener.java
@@ -4,16 +4,11 @@ import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.netty.buffer.ByteBufHelper;
 import com.github.retrooper.packetevents.protocol.nbt.NBT;
-import com.github.retrooper.packetevents.protocol.nbt.NBTLimiter;
-import com.github.retrooper.packetevents.protocol.nbt.serializer.DefaultNBTSerializer;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
-import com.github.retrooper.packetevents.resources.ResourceLocation;
 import com.github.retrooper.packetevents.wrapper.PacketWrapper;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientCustomClickAction;
-import me.hsgamer.hscore.logger.common.LogLevel;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
+import static com.github.retrooper.packetevents.wrapper.common.client.WrapperCommonClientCustomClickAction.MAX_PAYLOAD_SIZE;
 
 public class DialogCustomClickListener extends PacketListenerAbstract {
     private final BetterDialogs instance;
@@ -31,14 +26,7 @@ public class DialogCustomClickListener extends PacketListenerAbstract {
         byte[] byteArray = ByteBufHelper.copyBytes(buffer);
         PacketWrapper<?> wrapper = new PacketWrapper<>(cloneEvent);
         String dialogId = wrapper.readString();
-        byte[] nbtData = wrapper.readLengthPrefixed(65536, (ew) -> wrapper.readRemainingBytes());
-        NBT nbt;
-        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(nbtData); DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream)) {
-            nbt = DefaultNBTSerializer.INSTANCE.deserializeTag(NBTLimiter.noop(), dataInputStream, false);
-        } catch (Exception e) {
-            instance.getLogger().log(LogLevel.INFO, "Failed to read NBT data from custom click action", e);
-            return;
-        }
+        NBT nbt = wrapper.readLengthPrefixed(MAX_PAYLOAD_SIZE, PacketWrapper::readNBTRaw);
         cloneEvent.cleanUp();
 
         WrapperPlayClientCustomClickAction packet = new WrapperPlayClientCustomClickAction(event);
@@ -53,11 +41,5 @@ public class DialogCustomClickListener extends PacketListenerAbstract {
             byteArrayString.append(String.format("%02X ", b));
         }
         instance.getLogger().log(byteArrayString.toString());
-        instance.getLogger().log("NBT Byte Array: ");
-        StringBuilder nbtByteArrayString = new StringBuilder();
-        for (byte b : nbtData) {
-            nbtByteArrayString.append(String.format("%02X ", b));
-        }
-        instance.getLogger().log(nbtByteArrayString.toString());
     }
 }
