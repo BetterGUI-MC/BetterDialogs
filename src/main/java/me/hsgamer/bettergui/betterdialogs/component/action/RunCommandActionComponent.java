f2059c01a38e93b544951df6d9e8a22965d720f7
hsgamer <huynhqtienvtag@gmail.com>
2025-07-02 22:30:32 +0700

    support static and dynamic run command

--- src/main/java/me/hsgamer/bettergui/betterdialogs/component/action/RunCommandActionComponent.java
+++ src/main/java/me/hsgamer/bettergui/betterdialogs/component/action/RunCommandActionComponent.java
@@ -1,27 +1,40 @@
 package me.hsgamer.bettergui.betterdialogs.component.action;
 
+import com.github.retrooper.packetevents.protocol.chat.clickevent.RunCommandClickEvent;
 import com.github.retrooper.packetevents.protocol.dialog.action.Action;
 import com.github.retrooper.packetevents.protocol.dialog.action.DialogTemplate;
 import com.github.retrooper.packetevents.protocol.dialog.action.DynamicRunCommandAction;
+import com.github.retrooper.packetevents.protocol.dialog.action.StaticAction;
 import me.hsgamer.bettergui.betterdialogs.builder.DialogComponentBuilder;
-import me.hsgamer.hscore.common.MapUtils;
+import me.hsgamer.bettergui.util.StringReplacerApplier;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+import java.util.Objects;
 
 public class RunCommandActionComponent extends ActionComponent {
-    private final String template;
+    private final String command;
+    private final boolean isDynamic;
 
     public RunCommandActionComponent(DialogComponentBuilder.Input input) {
         super(input);
-        template = Optional.ofNullable(MapUtils.getIfFound(input.options(), "command", "template"))
-                .map(Object::toString)
-                .orElse("");
+        if (input.options().containsKey("command")) {
+            command = Objects.toString(input.options().get("command"));
+            isDynamic = false;
+        } else if (input.options().containsKey("template")) {
+            command = Objects.toString(input.options().get("template"));
+            isDynamic = true;
+        } else {
+            command = "";
+            isDynamic = false;
+        }
     }
 
     @Override
     protected @Nullable Action getAction(Player player) {
-        return new DynamicRunCommandAction(new DialogTemplate(template));
+        String replacedCommand = StringReplacerApplier.replace(command, player.getUniqueId(), this);
+        return isDynamic
+                ? new DynamicRunCommandAction(new DialogTemplate(replacedCommand))
+                : new StaticAction(new RunCommandClickEvent(replacedCommand));
     }
 }
