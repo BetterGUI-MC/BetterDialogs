8266a8985da7046cace52e229831ee4c0ce06324
hsgamer <huynhqtienvtag@gmail.com>
2025-07-02 18:50:58 +0700

    settings to modify dialog

--- src/main/java/me/hsgamer/bettergui/betterdialogs/menu/MultiActionDialogMenu.java
+++ src/main/java/me/hsgamer/bettergui/betterdialogs/menu/MultiActionDialogMenu.java
@@ -2,10 +2,27 @@ package me.hsgamer.bettergui.betterdialogs.menu;
 
 import me.hsgamer.bettergui.betterdialogs.BetterDialogs;
 import me.hsgamer.bettergui.betterdialogs.constructor.MultiActionDialogConstructor;
+import me.hsgamer.hscore.common.Validate;
 import me.hsgamer.hscore.config.Config;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 public class MultiActionDialogMenu extends DialogMenu<MultiActionDialogConstructor> {
+    private final int columns;
+
     public MultiActionDialogMenu(BetterDialogs instance, Config config) {
         super(instance, config, MultiActionDialogConstructor::create);
+        this.columns = Optional.ofNullable(menuSettings.get("columns"))
+                .map(Object::toString)
+                .flatMap(Validate::getNumber)
+                .map(Number::intValue)
+                .filter(columns -> columns > 0)
+                .orElse(2);
+    }
+
+    @Override
+    protected void modifyDialog(Player player, MultiActionDialogConstructor dialogConstructor) {
+        dialogConstructor.columns(columns);
     }
 }
--- src/main/java/me/hsgamer/bettergui/betterdialogs/menu/ServerLinksDialogMenu.java
+++ src/main/java/me/hsgamer/bettergui/betterdialogs/menu/ServerLinksDialogMenu.java
@@ -2,10 +2,35 @@ package me.hsgamer.bettergui.betterdialogs.menu;
 
 import me.hsgamer.bettergui.betterdialogs.BetterDialogs;
 import me.hsgamer.bettergui.betterdialogs.constructor.ServerLinksDialogConstructor;
+import me.hsgamer.hscore.common.MapUtils;
+import me.hsgamer.hscore.common.Validate;
 import me.hsgamer.hscore.config.Config;
+import org.bukkit.entity.Player;
+
+import java.util.Optional;
 
 public class ServerLinksDialogMenu extends DialogMenu<ServerLinksDialogConstructor> {
+    private final int columns;
+    private final int buttonWidth;
+
     public ServerLinksDialogMenu(BetterDialogs instance, Config config) {
         super(instance, config, ServerLinksDialogConstructor::create);
+        this.columns = Optional.ofNullable(menuSettings.get("columns"))
+                .map(Object::toString)
+                .flatMap(Validate::getNumber)
+                .map(Number::intValue)
+                .filter(columns -> columns > 0)
+                .orElse(2);
+        this.buttonWidth = Optional.ofNullable(MapUtils.getIfFound(menuSettings, "button-width", "width"))
+                .map(Object::toString)
+                .flatMap(Validate::getNumber)
+                .map(Number::intValue)
+                .filter(width -> width > 0)
+                .orElse(150);
+    }
+
+    @Override
+    protected void modifyDialog(Player player, ServerLinksDialogConstructor dialogConstructor) {
+        dialogConstructor.columns(columns).buttonWidth(buttonWidth);
     }
 }
